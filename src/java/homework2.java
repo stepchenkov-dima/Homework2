f07403797f41224eca246972e7b783b02553a851
dima <javacorese@gmail.com>
2023-09-27 17:10:21 +0200

    homework2

---
+++ src/java/homework2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class homework2 {
+    public static void main (String[] args){
+        /*Є прямокутник, в якого відома ширина w і висота h. Обчисліть і
+        виведіть в консоль периметр і площу заданого прямокутника.*/
+        System.out.println("Enter the width");
+        Scanner w = new Scanner(System.in);
+        int width = w.nextInt();
+        System.out.println("Enter the height");
+        Scanner h = new Scanner(System.in);
+        int height = h.nextInt();
+        System.out.printf("width = %d\nHeight = %d",width, height);
+        final int perimeter = 2 * (width + height);
+        System.out.printf("perimeter = %d",perimeter); //S = a x b
+        final int square = width * height;
+        System.out.printf("square = %d",square);
+    }
+}
